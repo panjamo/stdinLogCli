b9bd96edc7200d0a33af0f5ac0986cb9720fc99a
Jochen Schieberlein <jochen.schieberlein@thinprint.com>
2022-02-09 16:36:40 +0100

    minimize include files

--- stdinLogCli/stdinLogCli.cpp
+++ stdinLogCli/stdinLogCli.cpp
@@ -1,11 +1,9 @@
 // stdinLogCli.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #include <iostream>
 #include <string>
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
 #include <regex>
 
 int wmain(int argc, wchar_t* argv[])
